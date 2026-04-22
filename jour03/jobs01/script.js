08aec9aeb735b483c18dcf6b6ab3a663b56e99a0
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-04-22 10:23:37 +0200

    début jour3 jobs 01 fait

---
+++ jour03/jobs01/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#showButton").on("click", function () {
+        $("#citation").show();
+    });
+
+    $("#hideButton").on("click", function () {
+        $("#citation").hide();
+    });
+});
